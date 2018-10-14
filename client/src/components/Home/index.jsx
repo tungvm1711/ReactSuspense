0b8cd6c5595aa9bc51dde5b4ecc6933acdf2571c
tungvm1711 <9284010@gmail.com>
2018-10-14 14:44:38 +0700

    +creating MovieItem component

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import MovieItem from '../../components/MovieItem';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="container">
         <h1>The Movies Blog</h1>
+        <MovieItem />
+        <MovieItem />
       </div>
     );
   }
---
+++ client/src/components/MovieItem/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class MovieItem extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div class="divWrapper">
+                <h1>MovieTitle</h1>
+                <p>point</p>
+            </div>
+        );
+    }
+}
