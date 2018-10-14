cb91bfcebabd4fd4b0dfc2427c8d45d82610d122
tungvm1711 <9284010@gmail.com>
2018-10-14 14:29:51 +0700

    +adding homepage

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { withRouter, Switch, Route } from 'react-router-dom';
+
+import { Home } from '../../components';
 
 const App = (props) => {
   return (
-    <p>Hello App</p>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
   )
 }
 
-export default App;
+export default withRouter(App);
